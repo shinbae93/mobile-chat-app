d85429675ae71a5464593dc8e1a212fe43e71239
Hung Nguyen <hungquoc.nguyen@team.enouvo.com>
2022-06-11 22:08:46 +0700

    add adapter

--- app/src/main/java/com/midterm/realtimechatapp10/Fragments/UsersFragment.java
+++ app/src/main/java/com/midterm/realtimechatapp10/Fragments/UsersFragment.java
@@ -18,9 +18,9 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-import com.midterm.realtimechatapp.Adapter.UserAdapter;
-import com.midterm.realtimechatapp.Model.User;
-import com.midterm.realtimechatapp.R;
+import com.midterm.realtimechatapp10.Adapter.UserAdapter;
+import com.midterm.realtimechatapp10.Model.User;
+import com.midterm.realtimechatapp10.R;
 
 import java.util.ArrayList;
 import java.util.List;
